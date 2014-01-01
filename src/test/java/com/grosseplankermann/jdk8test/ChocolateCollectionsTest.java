864f299532fb9e97584e3e59b936f91fd4dddd02
holgergp <holgergp@gmail.com>
2014-01-01 19:22:10 +0100

    Readding sources

---
+++ src/test/java/com/grosseplankermann/jdk8test/ChocolateCollectionsTest.java
@@ -0,0 +1,33 @@
+package com.grosseplankermann.jdk8test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ChocolateCollectionsTest {
+    @Test
+    public void testJdk7way() throws Exception {
+        assertEquals(2,new ChocolateCollections().jdk7way() );
+    }
+
+    @Test
+    public void testJdk7wayWithAHintOfFunctions() throws Exception {
+        assertEquals(2,new ChocolateCollections().jdk7wayWithAHintOfFunctions() );
+    }
+
+    @Test
+    public void testJdk8wayWithClosures() throws Exception {
+        assertEquals(2,new ChocolateCollections().jdk8wayWithClosures() );
+    }
+
+    @Test
+    public void testJdk8wayWithAnonymousClassesAndStreamsAPI() throws Exception {
+        assertEquals(2,new ChocolateCollections().jdk8wayWithAnonymousClassesAndStreamsAPI() );
+    }
+
+    @Test
+    public void testJdk8wayWithClosuresAndStreamsAPI() throws Exception {
+        assertEquals(2,new ChocolateCollections().jdk8wayWithClosuresAndStreamsAPI() );
+    }
+}
